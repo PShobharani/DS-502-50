b4afc555faab78d44844c0e7e85e129f133f587b
PShobharani <143234265+PShobharani@users.noreply.github.com>
2023-09-06 15:46:16 -0400

    my information v1

---
+++ MyInfo.py
@@ -0,0 +1,13 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # DS502-50
+... 
+... #Shobharani Polasa
+... 
+... #myInfo
+>>> print("Shobharani Polasa")
+Shobharani Polasa
+>>> print("100 Pier village, Long branch, NJ, 07740")
+100 Pier village, Long branch, NJ, 07740
+>>> print("9494949492")
+9494949492
