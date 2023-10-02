c5fe263f3b6bba2b76fc7be3d5277cbf75e75f68
PShobharani <143234265+PShobharani@users.noreply.github.com>
2023-10-02 16:52:04 -0400

    restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -7,15 +7,15 @@ vegan = False
 gluten_free = False
 
 
-is_vegetarian = input('Is anyone in your party vegetarian?')
+is_vegetarian = str(input('Is anyone in your party vegetarian?')).lower()
 if is_vegetarian == 'yes' or is_vegetarian == 'Yes':
     vegetarian = True
     
-is_vegan = input('Is anyone in your party vegan?')
+is_vegan = str(input('Is anyone in your party vegan?')).lower()
 if is_vegan == 'yes' or is_vegan == 'Yes':
     vegan = True
     
-is_gluten_free = input('Is anyone in your party gluten free?')
+is_gluten_free = str(input('Is anyone in your party gluten free?')).lower()
 if is_gluten_free == 'yes' or is_gluten_free == 'Yes':
     gluten_free = True
 
