0274d6b67af9963c60aeed309dd99022d3587bb0
PShobharani <143234265+PShobharani@users.noreply.github.com>
2023-10-05 15:10:35 -0400

    restaurant_V2.py

--- restaurant.py
+++ restaurant.py
@@ -1,34 +1,38 @@
 # DS502-50 - Introduction to Computer Science
 # Shobharani Polasa
-# restaurant
+# restaurant version 2
 
-vegetarian = False
-vegan = False
-gluten_free = False
-
-
-is_vegetarian = str(input('Is anyone in your party vegetarian?')).lower()
-if is_vegetarian == 'yes' or is_vegetarian == 'Yes':
-    vegetarian = True
+keepGoing = "yes"
+while keepGoing == "yes":
+    vegetarian = False
+    vegan = False
+    gluten_free = False
     
-is_vegan = str(input('Is anyone in your party vegan?')).lower()
-if is_vegan == 'yes' or is_vegan == 'Yes':
-    vegan = True
+    is_vegetarian = str(input('Is anyone in your party vegetarian? ')).lower()
+    if is_vegetarian == 'yes':
+        vegetarian = True
+        
+    is_vegan = str(input('Is anyone in your party vegan? ')).lower()
+    if is_vegan == 'yes':
+        vegan = True
+        
+    is_gluten_free = str(input('Is anyone in your party gluten free? ')).lower()
+    if is_gluten_free == 'yes':
+        gluten_free = True
+
+    print('Here are your restaurant choices: ')
+
+    if vegetarian == False and vegan == False and gluten_free == False:
+        print('Joe\'s Gourmet Burgers')
+
+    if vegan == False and gluten_free == False:
+        print('Mama\'s Fine Italian')
+
+    if vegan == False:
+        print('Main Street Pizza')
+
+    print('Corner Cafe')
+    print('Chef\'s Kitchen')
     
-is_gluten_free = str(input('Is anyone in your party gluten free?')).lower()
-if is_gluten_free == 'yes' or is_gluten_free == 'Yes':
-    gluten_free = True
-
-print('Here are your resaurant choices:')
-
-if vegetarian == False and vegan == False and gluten_free == False:
-    print('Joe\'s Gourmet Burgers')
-
-if vegan == False and gluten_free == False:
-    print('Mama\'s Fine Italian')
-
-if vegan == False:
-    print('Main Street Pizza')
-
-print('Corner Cafe')
-print('Chef\'s Kitchen')
+    keepGoing = input("Do you want to keep going(yes/no)? ")
+print("Thanks for searching for a restaurant!")
