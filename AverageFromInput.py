80ec279991d8a51cdf1fb49cb3341d76ffbcffb0
PShobharani <143234265+PShobharani@users.noreply.github.com>
2023-12-03 16:57:02 -0500

    AverageFromInput.py

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -2,31 +2,50 @@
 # Shobharani Polasa
 # AverageFromInput File with Exception Handling
 
+import random
+
+def generate_random_value():
+    return random.uniform(1, 100)  # Generates a random float between 1 and 100
+
 def main():
     try:
         total, count = sumNumbers()
         averageNumbers(total, count)
-    except IOError:
-        print("The file could not be opened.")
+    except FileNotFoundError:
+        print("The file could not be found.")
     except ValueError:
         print("Unexpected data. Please only input numbers.")
     except:
-        print("An error occured when trying to execute the program.")
-        
+        print("An error occurred when trying to execute the program.")
+
 def sumNumbers():
-    infile = open('numbers.txt', 'r')
-    total = 0
-    count = 1
-    for line in infile:
-        number = float(line)
-        total += number
-        print(f"I read in {count} number(s) "f"Current number is:     {number:.2f}"f"  Total is:  {total:.2f}")
-        count+=1
-    return total, count
+    try:
+        infile = open('numbers.txt', 'r')
+        total = 0
+        count = 1
+        for line in infile:
+            try:
+                number = float(line)
+                total += number
+                print(f"I read in {count} number(s). Current number is: {number:.2f} Total is: {total:.2f}")
+                count += 1
+            except ValueError:
+                # If conversion fails, assign a random value
+                number = generate_random_value()
+                print(f"Error: Cannot convert '{line.strip()}' to a number. Assigned a random value: {number:.2f}")
+                total += number
+                count += 1
+        infile.close()
+        return total, count
+    except FileNotFoundError:
+        raise FileNotFoundError
 
 def averageNumbers(total, count):
-    average = total/(count-1)
-    print(f'Average: {average:.1f}')
+    try:
+        average = total / (count - 1)
+        print(f'Average: {average:.1f}')
+    except ZeroDivisionError:
+        print("Error: Cannot calculate the average of 0 numbers.")
 
 # Call the main function.
 if __name__ == '__main__':
