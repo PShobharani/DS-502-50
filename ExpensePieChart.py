b5fc0da7dc1ebc716d94a376abade4925e495aaa
PShobharani <143234265+PShobharani@users.noreply.github.com>
2023-12-03 16:30:15 -0500

    ExpensePieChart.py

--- ExpensePieChart.py
+++ ExpensePieChart.py
@@ -7,6 +7,13 @@
 #pip install matplotlib
 
 import matplotlib.pyplot as plt
+import random
+
+import matplotlib.pyplot as plt
+import random
+
+def generate_random_value():
+    return random.randint(100, 1000)  # Generates a random value between 100 and 1000
 
 def main():
     # initialize variables
@@ -22,22 +29,21 @@ def main():
                 if len(line) > 0: 
                     label, cost = line.split('\t')
                     try:
-                        # Try to convert cost to an integer, if it fails, use 0
+                        # Try to convert cost to an integer, if it fails, use 0 or generate a random value
                         amount = int(cost)
                     except ValueError:
                         # If conversion fails, assign 0 as the default value
-                        amount = 0
-                        print(f"Error: '{cost}' is not a valid integer. Converted '{cost}' to an integer and assigned 0.")
+                        # For text entries, generate a random value
+                        amount = generate_random_value()
+                        print(f"Error: '{cost}' is not a valid integer. Assigned a random value: {amount}")
 
                     categories.append(label)
                     amounts.append(amount)
 
         # create pie chart
-        # We can change the chart name and pie slice colors as well
-        # autopct='%.2f%%' show the pecentage of slice
         if len(amounts) > 0:
-            plt.pie(amounts, labels=categories, colors=('blue', 'orange', 'green', 'red', 'magenta', 'brown', 'cyan'), autopct='%.2f%%')
-            #plt.pie(amounts, labels=categories, colors=('blue', 'orange', 'green', 'red', 'magenta', 'brown', 'cyan'))
+            colors = ('blue', 'orange', 'green', 'red', 'magenta', 'brown', 'cyan')
+            plt.pie(amounts, labels=categories, colors=colors, autopct='%.2f%%')
             plt.title('Monthly Expenses')
             plt.show()
         else:
@@ -48,9 +54,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
